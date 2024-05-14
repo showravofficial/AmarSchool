4910a7dbca7ad9ad5d040408005ad9fe208b7337
Mir Md. Mosarof Hossan Showrav <showravofficial@gmail.com>
2024-05-14 15:55:50 +0600

    initial level

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:amar_school/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white
+      ),
+      title: "Amar School",
+      home: WelcomeScreen(),
+    );
+  }
+}
