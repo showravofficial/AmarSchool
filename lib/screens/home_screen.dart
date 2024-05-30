1a6344c5aac8975d32eeea1731832bea79ae7b59
Mir Md. Mosarof Hossan Showrav <showravofficial@gmail.com>
2024-05-31 02:23:24 +0600

    Primary level completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,258 @@
 import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  List catName = [
+    "Category",
+    "Classes",
+    "Free Course",
+    "Book Store",
+    "Live Course",
+    "Leader Board",
+  ];
 
-class HomePage extends StatelessWidget{
+  List<Color> catColor = [
+    Color(0xff00FFFF),
+    Color(0xff7B68EE),
+    Color(0xff008B8B),
+    Color(0xff66CDAA),
+    Color(0xff7CFC00),
+    Color(0xff800080),
+  ];
+
+  List<Icon> catIcons = [
+    Icon(
+      Icons.category,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.video_library,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.assignment,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.store,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.play_circle_fill,
+      color: Colors.white,
+      size: 30,
+    ),
+    Icon(
+      Icons.emoji_events,
+      color: Colors.white,
+      size: 30,
+    ),
+  ];
+
+  List imgList = [
+    "Dart_logo.png",
+    "Flutter_logo.png",
+    "c++.png",
+    "python.jpeg",
+    "java.png",
+  ];
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.greenAccent,
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 28, 204, 187),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    // Icon(
+                    //   Icons.dashboard,
+                    //   size: 30,
+                    //   color: Colors.white,
+                    // ),
+                    Container(
+                      height: 60.0,
+                      width: 60.0,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(70),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2.0,
+                          ),
+                          image: DecorationImage(
+                              image: AssetImage("assets/images/profile.png"))),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.notifications,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                        Icon(Icons.menu,
+                        size: 30,
+                        color: Colors.white,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Hi, Showrav",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                        color: Colors.white),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(top: 5, bottom: 20),
+                  height: 55,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Type here to  Search",
+                        hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 25,
+                        )),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: catName.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: catColor[index],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: catIcons[index],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          catName[index],
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(0.6)),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Courses",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color:
+                              Color.fromARGB(255, 9, 54, 235).withOpacity(0.7)),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: imgList.length,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio:
+                        (MediaQuery.of(context).size.height - 50 - 25) /
+                            (4 * 240),
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 10,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white.withOpacity(0.5),
+                            border: Border.all(color: Colors.black)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                  "assets/images/${imgList[index]}"),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
